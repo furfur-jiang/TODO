0f802316de19d9fdf615380986aa451407d1f444
wu_monkey <7367930+wu_monkey@user.noreply.gitee.com>
2020-05-27 13:47:53 +0800

    跑步页面完成

--- miniprogram/pages/run/run.js
+++ miniprogram/pages/run/run.js
@@ -4,13 +4,18 @@
 var countTooGetLocation = 0;
 var total_micro_second = 0;
 var starRun = 0;
+var endRun=1;
 var totalSecond  = 0;
 var oriMeters = 0.0;
 /* 毫秒级倒计时 */
 function count_down(that) {
 
-    if (starRun == 0) {
-      return;
+    if (starRun == 0 || endRun == 1) {
+      if(endRun===1){
+        console.log(date_format(total_micro_second));
+        total_micro_second=0;
+      }
+        return;
     }
 
     if (countTooGetLocation >= 100) {
@@ -22,14 +27,12 @@ function count_down(that) {
         that.getLocation();
         countTooGetLocation = 0;
     }  
-
-    setTimeout(function(){
-		countTooGetLocation += 10;
-    total_micro_second += 10;
-		count_down(that);
-    }
-    ,10
-    )
+    
+    setTimeout(function () {
+      countTooGetLocation += 10;
+      total_micro_second += 10;
+      count_down(that);
+    }, 10);
   }
 
 
@@ -77,18 +80,22 @@ Page({
     markers: [],
     covers: [],
     meters: 0.00,
-    time: "0:00:00"
+    time: "0:00:00",
+    start_run:true,
+    stop_run:false,
+    end_run:false,
+    cit_run:false
   },
 
 //****************************
-  onLoad:function(options){
+  onLoad(options){
     // 页面初始化 options为页面跳转所带来的参数
     this.getLocation()
     // console.log("onLoad")
     count_down(this);
   },
   //****************************
-  openLocation:function (){
+  openLocation (){
     wx.getLocation({
       type: 'gcj02', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
       success: function(res){
@@ -103,26 +110,61 @@ Page({
 
 
 //****************************
-  starRun :function () {
+  starRun () {
+    this.setData({
+      start_run: false,
+      stop_run: true,
+      end_run: true,
+      cit_run: false
+    });
     if (starRun == 1) {
       return;
     }
     starRun = 1;
+    endRun=0;
     count_down(this);
     this.getLocation();
   },
 
 
  //****************************
-  stopRun:function () {
+  stopRun() {
     starRun = 0;
+    this.setData({
+      start_run: false,
+      stop_run: false,
+      end_run: true,
+      cit_run: true,
+    });
     count_down(this);
   },
 
 
 //****************************
-  updateTime:function (time) {
-
+endRun(){
+  this.setData({
+    start_run: true,
+    stop_run: false,
+    end_run: false,
+    cit_run: false,
+  });
+  starRun=0;
+  endRun=1;
+  count_down(this);
+},
+citRun(){
+  starRun=1;
+  this.setData({
+    start_run: false,
+    stop_run: true,
+    end_run: true,
+    cit_run: false,
+  });
+  count_down(this);
+},
+// *******************************************
+  updateTime(time) {
+    console.log('更新时间······')
     var data = this.data;
     data.time = time;
     this.data = data;
@@ -134,7 +176,7 @@ Page({
 
 
 //****************************
-  getLocation:function () {
+  getLocation() {
     var that = this
     wx.getLocation({
 
